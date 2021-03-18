c9c5edd65aa8334e3822850bb7741381d4b57db3
advya <50411324+Advya@users.noreply.github.com>
2021-03-18 13:13:06 +0200

    Display error validation

--- app.rb
+++ app.rb
@@ -29,7 +29,10 @@ end
 post '/visit' do	
 
 	c = Client.new params[:client]
-	c.save
-
-	erb "<h2>Спасибо что записались</h2>"
+	if c.save
+		erb "<h2>Спасибо что записались</h2>"
+	else 
+		@error = c.errors.full_messages.first
+		erb :visit
+	end
 end
